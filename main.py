2d560829a0d052da332ad6ee9ab8e54ff85a5000
Cameron <cameronlooney18@gmail.com>
2023-02-24 22:50:31 +0000

    added requirements file

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title='CADLAE', page_icon=':robot_face:')
 # Set page header
 st.header('CADLAE Framework Demo')
 
-df = pd.read_csv('/Users/cameronlooney/PycharmProjects/CADLAE_streamlit/test_data.csv')
+df = pd.read_csv('test_data.csv')
 # Add some text to the page
 st.write('This is a basic example of a Streamlit web page.')
 
