31c760f5f8d5fb254d223a384483f4073db58917
Cameron <cameronlooney18@gmail.com>
2023-02-24 22:55:19 +0000

    check if csv loads

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ st.set_page_config(page_title='CADLAE', page_icon=':robot_face:')
 # Set page header
 st.header('CADLAE Framework Demo')
 
-df = pd.read_csv('test_data.csv')
+df = pd.read_csv('./test_data.csv')
+st.write(df.head())
 # Add some text to the page
-st.write('This is a basic example of a Streamlit web page.')
+st.write('This is lit web page.')
 
 # Run the app
