87d62a196fca4f987d632407718a85299441ee94
Cameron <cameronlooney18@gmail.com>
2023-02-25 11:10:41 +0000

    added plots to model report

--- main.py
+++ main.py
@@ -55,8 +55,10 @@ def make_predictions():
     import torch
     from cadlae.detector import AnomalyDetector
     from cadlae.preprocess import DataProcessor
-    from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix, accuracy_score
+    from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix, accuracy_score, classification_report
     import numpy as np
+
+
     import matplotlib.pyplot as plt
     import seaborn as sns
     import time
@@ -149,13 +151,17 @@ def make_predictions():
                                 'Result': [accuracy, precision, recall, f1, roc]})
         st.table(metrics)
         
+        st.subheader('Classification Report 📝')
+        report = classification_report(y_test, y_pred, output_dict=True)
+        report = pd.DataFrame(report).transpose()
+        st.table(report)
         
         st.subheader('Confusion Matrix')
       
 
         # plot confusion matrix
         def make_confusion_matrix(y_true, y_prediction, c_map="viridis"):
-            sns.set(font_scale=1.2)
+            sns.set(font_scale=0.8)
             fig, ax = plt.subplots()
             ax.set_title('CADLAE Confusion Matrix')
             cm = confusion_matrix(y_true, y_prediction)
@@ -166,6 +172,118 @@ def make_predictions():
             sns.heatmap(cm_matrix, annot=True, fmt='.0f', cmap=c_map, linewidths=1, linecolor='black', clip_on=False)
             st.pyplot(fig)
         make_confusion_matrix(y_test,y_pred, c_map = "Blues")
+
+        st.subheader('ROC-AUC Curve')
+        def plot_roc_curve(fpr, tpr):
+            fig, ax = plt.subplots()
+            ax.set_facecolor('white')  # set background color to white
+            ax.spines['bottom'].set_color('black')  # set color of x-axis to black
+            ax.spines['left'].set_color('black')
+            plt.plot(fpr, tpr, color='orange', label='ROC')
+            plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
+            plt.xlabel('False Positive Rate')
+            plt.ylabel('True Positive Rate')
+            plt.title('Receiver Operating Characteristic (ROC) Curve')
+            st.pyplot(fig)
+
+        fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+        plot_roc_curve(fpr, tpr)
+
+        def get_attacks(y_test, outlier=1, normal=0, breaks=[]):
+            '''
+            Get indices of anomalies
+            :param y_test: predictions from semi supervised model
+            :param outlier: label for anomalies
+            :param normal: label for normal data points
+            :param breaks: indices of breaks in data
+            :return:
+            '''
+            events = dict()
+            label_prev = normal
+            event = 0  # corresponds to no event
+            event_start = 0
+            for tim, label in enumerate(y_test):
+                if label == outlier:
+                    if label_prev == normal:
+                        event += 1
+                        event_start = tim
+                    elif tim in breaks:
+                        # A break point was hit, end current event and start new one
+                        event_end = tim - 1
+                        events[event] = (event_start, event_end)
+                        event += 1
+                        event_start = tim
+        
+                else:
+                    # event_by_time_true[tim] = 0
+                    if label_prev == outlier:
+                        event_end = tim - 1
+                        events[event] = (event_start, event_end)
+                label_prev = label
+    
+            if label_prev == outlier:
+                event_end = tim - 1
+                events[event] = (event_start, event_end)
+            return events
+
+        
+
+        def get_attack_idx_list(dictionary):
+            '''
+            Get list of indices of anomalies
+            :param dictionary: dictionary of anomalies
+            :return: Dictionary of anomalies, value is changed from (start, end) to list of indices
+            '''
+            for key, value in dictionary.items():
+                if isinstance(value, tuple):
+                    dictionary[key] = list(range(value[0], value[1] + 1))
+            return dictionary
+
+    
+        dict_attacks = get_attacks(y_pred, outlier=1, normal=0, breaks=[])
+        attacks = get_attack_idx_list(dict_attacks)
+
+        import matplotlib.pyplot as plt
+        st.subheader('Predicted Anomalies')
+        def plot_anomalies(df, column, anomalies, scaler=None):
+            '''
+            Plot anomalies
+            :param df: dataframe to plot
+            :param column: column to plot
+            :param anomalies: dictionary of anomalies -> pass through dictionary - list pipeline to get dictionary with indx of anomalies
+            :param reverse_scaler: object used to scale the data -> reverses to original scale in plot of passed
+            '''
+            fig, ax = plt.subplots()
+            ax.set_facecolor('white')  # set background color to white
+            ax.spines['bottom'].set_color('black')  # set color of x-axis to black
+            ax.spines['left'].set_color('black')
+            if scaler is not None:
+                df = pd.DataFrame(scaler.inverse_transform(df), columns=df.columns)
+            title = "Plot of {}".format(column)
+            ax.plot(df[column])
+            ax.set_title(title)
+            for key, value in anomalies.items():
+                ax.plot(value, df[column][value], 'ro', markersize=4,color = 'red')
+            st.pyplot(fig)
+
+        plot_anomalies(X_test, "XMV(10)", attacks, scaler)
+
+        st.subheader('LSTM Reconstruction Error')
+        def plot_reconstructions(details, X, column):
+            fig, ax = plt.subplots()
+            ax.set_facecolor('white')  # set background color to white
+            ax.spines['bottom'].set_color('black')  # set color of x-axis to black
+            ax.spines['left'].set_color('black')
+            ax.plot(X[column], label='original series')
+            col_idx = X.columns.get_loc(column)
+            ax.plot(details['errors_mean'][col_idx], label='reconstructed error mean',color = 'red')
+            ax.set_title('Reconstructions of column {}'.format(column))
+            st.pyplot(fig)
+
+        plot_reconstructions(details, X_test, "XMV(10)")
+    
+
+        
 
       
 
